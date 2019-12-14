dcd9859aa8151b0c35aa11e1e6637f874cad40bc
xalkan <shahidalyk@gmail.com>
2019-12-14 18:25:48 +0500

    use axios to get crypto price

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+const axios = require('axios')
 
 const notifyBtn = document.getElementById('notifyBtn')
+let price = document.getElementById('price')
+let targetPrice = document.getElementById('targetPrice')
 
 notifyBtn.addEventListener('click', function(e){
     const modalPath = path.join('file://', __dirname, 'add.html')
@@ -22,3 +25,16 @@ notifyBtn.addEventListener('click', function(e){
     win.loadURL(modalPath)
     win.show()
 })
+
+function getBTC(){
+    axios.get('http://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+        .then(res => {
+            const cryptos = res.data.BTC.USD
+            price.innerHTML = '$'+ cryptos.toLocaleString('en')
+        })
+}
+
+
+setInterval(() => {
+    getBTC()
+}, 3000);
