79832e0f8c183464fdb3a1be11d0a142d08478e3
xalkan <shahidalyk@gmail.com>
2019-12-14 17:57:36 +0500

    added new window to adjust notification price

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+
+const notifyBtn = document.getElementById('notifyBtn')
+
+notifyBtn.addEventListener('click', function(e){
+    const modalPath = path.join('file://', __dirname, 'add.html')
+    let win = new BrowserWindow({ 
+        frame: false, 
+        width: 400, 
+        height: 200,
+        transparent:true
+    })
+    
+    win.on('close', function() { win == null })
+    
+    win.loadURL(modalPath)
+    win.show()
+})
